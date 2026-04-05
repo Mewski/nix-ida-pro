f4aa12711786286475c2988630dc5734ac754e94
Mewski <mewski@mewski.dev>
2026-04-04 23:12:51 -0500

    feat: make hm run idapyswitch

--- hm-module.nix
+++ hm-module.nix
@@ -1,5 +1,5 @@
 { self, ... }:
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.programs.ida-pro;
 in
@@ -11,5 +11,9 @@ in
   config = lib.mkIf cfg.enable {
     nixpkgs.overlays = [ self.overlays.default ];
     home.packages = [ cfg.package ];
+
+    home.activation.idapyswitch = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      ${cfg.package}/opt/ida-pro/idapyswitch -s ${lib.getLib pkgs.python3}/lib/libpython3.${lib.versions.majorMinor pkgs.python3.version}.so 2>/dev/null || true
+    '';
   };
 }
